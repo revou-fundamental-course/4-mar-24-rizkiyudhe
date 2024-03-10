e6c3317717b21ae2aa6dea789dc88562e7c94ce8
rizkiyudhe <rizkiyudhe22@gmail.com>
2024-03-11 02:27:36 +0700

    Button "Reset" OK, Tinggal finishing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
+// button hitung BMI
+function validasi() {
+  var beratbadan = document.getElementById("bb").value;
+  var usia = document.getElementById("u").value;
+  var tinggibadan = document.getElementById("tb").value;
+
+  // Cek apakah nilai teks field kosong
+  if (beratbadan.trim() === "") {
+    alert("Silakan isi text field terlebih dahulu!");
+    return false; // Menghentikan proses
+  } else if (usia.trim() === "") {
+    alert("Silakan isi text field terlebih dahulu!");
+    return false; // Menghentikan proses
+  } else if (tinggibadan.trim() === "") {
+    alert("Silakan isi text field terlebih dahulu!");
+    return false; // Menghentikan proses
+  }
+
+  return true; // Mengizinkan proses
+}
+
 function hitung() {
+  if (!validasi()) {
+    return; // Hentikan proses jika validasi gagal
+  }
+
   let bb = parseFloat(document.getElementById("bb").value);
   let tb = parseFloat(document.getElementById("tb").value);
   let bmi = bb / (tb * tb);
@@ -6,15 +31,24 @@ function hitung() {
   var ktr;
   document.getElementById("hasil").innerHTML = hasil;
 
-  if (bmi < 18.15) {
+  if (hasil < 18.15) {
     ktr = "Kekurangan Berat Badan";
-  } else if ((bmi) => 18.15 && bmi <= 24.9) {
+  } else if (hasil >= 18.15 && hasil <= 24.9) {
     ktr = "Normal (ideal)";
-  } else if ((bmi) => 25.0 && bmi <= 29.9) {
+  } else if (hasil >= 25.0 && hasil <= 29.9) {
     ktr = "Kelebihan Berat Badan";
   } else {
     ktr = "Obesitas (kegemukan)";
   }
 
   document.getElementById("keterangan").innerHTML = ktr;
+}
+
+// button Reset
+function reset() {
+  document.getElementById("bb").value = " ";
+  document.getElementById("u").value = " ";
+  document.getElementById("tb").value = " ";
+  document.getElementById("hasil").innerHTML = "0";
+  document.getElementById("keterangan").innerHTML = "-";
 }
